1a6ad677fd4360f034912deaf53ad426632f955f
lh <jyfc7879@gmail.com>
2016-06-29 17:13:47 +0800

    项目依赖 完成 添加 .gitignore spring 基本包依赖添加 启动服务 访问http://localhost:8080/hello
    基本架构完成 step

---
+++ spring-application/src/main/java/com/chinal/lh/application/controller/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.chinal.lh.application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liuhua on 16-6-29.
+ */
+@RestController
+public class HelloWorld {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String greeting() {
+        return "Hello World!";
+    }
+}
---
+++ spring-application/src/main/java/com/chinal/lh/application/fdsa.java
@@ -0,0 +1,7 @@
+package com.chinal.lh.application;
+
+/**
+ * Created by liuhua on 16-6-29.
+ */
+public class fdsa {
+}
