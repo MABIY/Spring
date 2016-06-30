8b2043384a21067a2dda279106e09f427b3d9ef0
lh <jyfc7879@gmail.com>
2016-06-30 10:52:25 +0800

    change

--- spring-web/src/main/java/com/chinal/lh/spring/web/spring/WebAppInitializer.java
+++ spring-web/src/main/java/com/chinal/lh/spring/web/spring/WebAppInitializer.java
@@ -2,8 +2,11 @@ package com.chinal.lh.spring.web.spring;
 
 import org.springframework.cglib.proxy.Dispatcher;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -13,13 +16,37 @@ import javax.servlet.ServletRegistration;
  */
 //Implementations of this SPI will be detected automatically by  SpringServletContainerInitializer (在项目中随意放置)
 //实现类 WebAppInitializer 效果等同于web.xml
-public class WebAppInitializer implements WebApplicationInitializer {
-    public void onStartup(javax.servlet.ServletContext container) throws ServletException {
-        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-        appContext.setConfigLocation("/WEB-INF/spring/dispatcher-config.xml");
+public class WebAppInitializer extends AbstractDispatcherServletInitializer {
 
-        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(appContext));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
+    //TODO  添加 parent context
+//    public void onStartup(javax.servlet.ServletContext container) throws ServletException {
+//        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+//        appContext.setConfigLocation("/WEB-INF/spring/dispatcher-config.xml");
+//
+//        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(appContext));
+//        dispatcher.setLoadOnStartup(1);
+//        dispatcher.addMapping("/");
+//    }
+
+    @Override
+    protected WebApplicationContext createRootApplicationContext() {
+        return null;
     }
+
+    @Override
+    protected WebApplicationContext createServletApplicationContext() {
+        XmlWebApplicationContext cxt = new XmlWebApplicationContext();
+        cxt.setConfigLocation("/WEB-INF/spring/dispatcher-config.xml");
+        return cxt;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+
+
+
+
 }
