7bb55058ba71568fe048b62e5b8a6b4fb66a5a6f
lh <jyfc7879@gmail.com>
2016-07-01 16:33:08 +0800

    delete

---
+++ spring-infrastructure/src/main/java/com/chinal/lh/infrastructure/mapper/AccountMapper.java
@@ -0,0 +1,16 @@
+package com.chinal.lh.infrastructure.mapper;
+
+import com.chinal.lh.domain.data.Account;
+import java.util.List;
+
+public interface AccountMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Account record);
+
+    Account selectByPrimaryKey(Integer id);
+
+    List<Account> selectAll();
+
+    int updateByPrimaryKey(Account record);
+}
