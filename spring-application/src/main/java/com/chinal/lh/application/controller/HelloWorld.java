1a72170fe038948e9f94f3baefe6a742730a9a10
lh <jyfc7879@gmail.com>
2016-07-01 21:27:00 +0800

    在父 pom 中 对pluginManagement 做了修改

--- spring-application/src/main/java/com/chinal/lh/application/controller/HelloWorld.java
+++ spring-application/src/main/java/com/chinal/lh/application/controller/HelloWorld.java
@@ -1,9 +1,7 @@
 package com.chinal.lh.application.controller;
 
 import com.chinal.lh.domain.Repository.AccountRepository;
-import com.chinal.lh.domain.Repository.UserRepository;
 import com.chinal.lh.domain.data.Account;
-import com.chinal.lh.domain.data.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
@@ -18,7 +16,7 @@ public class HelloWorld {
     @Autowired
     AccountRepository accountRepository;
 
-    @RequestMapping(value = "/hello/{id}",method = RequestMethod.GET)
+    @RequestMapping(value = "/hello/{id}", method = RequestMethod.GET)
     public Account greeting(@PathVariable int id) {
         return accountRepository.find(id);
     }
